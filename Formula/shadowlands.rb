9498cfa4c3e01904dc749a2038bbc5339218636f
C. Thomas <cthomas@railjumper.com>
2018-09-24 17:06:00 -0500

    Small upgrades to script

--- Formula/shadowlands.rb
+++ Formula/shadowlands.rb
@@ -4,8 +4,8 @@
 class Shadowlands < Formula
   desc "Shadowlands public Ethereum terminal"
   homepage "https://github.com/kayagoban/shadowlands"
-  url "https://github.com/kayagoban/shadowlands/archive/v0.06a.tar.gz"
-  sha256 "ab038327240151164a9794cd8e53732ea8fe4b9a2cb8bb870b0d1db32f8a8575"
+  url "https://github.com/kayagoban/shadowlands/archive/v0.07a.tar.gz"
+  sha256 "16d5f2a638157c1b1412b9a51df4bcdcb85651c4215f64ea6d77757847b9e7f2"
 
   depends_on "python3" => :recommended
 
