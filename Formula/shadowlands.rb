12a839bb321dae93ee298315894a32709fb4e551
C. Thomas <cthomas@soykaf.digital>
2019-06-12 16:50:48 -0500

    wip

--- Formula/shadowlands.rb
+++ Formula/shadowlands.rb
@@ -6,7 +6,7 @@ class Shadowlands < Formula
   homepage "https://github.com/kayagoban/shadowlands"
   url 'https://github.com/kayagoban/shadowlands/archive/1.0.3.tar.gz'
   sha256 '6abedc18040485c88b99c01eacae96296efc1d552705030e91843406bb95e73f'
-  version "1.0.5"
+  version "1.0.6"
 
   #depends_on "zbar"
   #depends_on "opencv"
@@ -19,7 +19,7 @@ class Shadowlands < Formula
 
   def install
     lib.install Dir["*"]
-    bin.install "scripts/shadowlands"
+    bin.install "output/scripts/shadowlands"
   end
 
 end
