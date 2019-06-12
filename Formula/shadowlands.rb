7a4a2a5641b336fe45649d5434af01ea3484c811
C. Thomas <cthomas@soykaf.digital>
2019-06-12 18:22:28 -0500

    wip

--- Formula/shadowlands.rb
+++ Formula/shadowlands.rb
@@ -8,7 +8,7 @@ class Shadowlands < Formula
   homepage "https://github.com/kayagoban/shadowlands"
   url 'https://github.com/kayagoban/shadowlands/archive/1.0.4.tar.gz'
   sha256 '87b1578dabdcc41aa8d3ef9f585f0490acd02130797f6c802a794f3897f7d521'
-  version "1.0.4"
+  version "1.0.5"
 
   depends_on "python3" => :recommended
 
