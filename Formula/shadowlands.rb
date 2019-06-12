9c78cab85f902cc7118be32b6b76d91d3624a665
C. Thomas <cthomas@soykaf.digital>
2019-06-12 17:03:59 -0500

    wip

--- Formula/shadowlands.rb
+++ Formula/shadowlands.rb
@@ -1,12 +1,15 @@
+require irb
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+#
+#
 class Shadowlands < Formula
   desc "Shadowlands public Ethereum terminal"
   homepage "https://github.com/kayagoban/shadowlands"
   url 'https://github.com/kayagoban/shadowlands/archive/1.0.3.tar.gz'
   sha256 '6abedc18040485c88b99c01eacae96296efc1d552705030e91843406bb95e73f'
-  version "1.0.8"
+  version "1.0.9"
 
   #depends_on "zbar"
   #depends_on "opencv"
@@ -16,6 +19,7 @@ class Shadowlands < Formula
   
 
   def install
+    irb
     lib.install Dir["*"]
     bin.install Dir["scripts/*"]
   end
